8f796bedc732258db495d3f131021eeb60c1d63f
wa5 <47422872+wa5@users.noreply.github.com>
2021-10-10 23:03:20 -0700

    bhdsjh

---
+++ fullstack/frontnd/languages/js/vanill-js/intro/script.js
@@ -0,0 +1,5 @@
+function calu(){
+    alert("hello im in page js:out put on alert1")
+    document.write('<h1>hello out put on document 2</h1>')
+    console.log("im out put on console 3rd wway")
+}
