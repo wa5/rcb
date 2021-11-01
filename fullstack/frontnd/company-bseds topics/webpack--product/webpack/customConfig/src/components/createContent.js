aeefd54687e4901382b8156def1562361aa9ed42
wa5 <waseemahmes116@gmail.com>
2021-11-01 07:34:25 +0530

    ffddfdf

--- fullstack/frontnd/company-bseds topics/webpack--product/webpack/customConfig/src/components/createContent.js
+++ fullstack/frontnd/company-bseds topics/webpack--product/webpack/customConfig/src/components/createContent.js
@@ -6,10 +6,11 @@ function createContent() {
     let m=document.createElement('main')
     let p=document.createElement('p')
    let img=document.createElement('img')
-   main.append(img)
+   m.append(p)
    p.append(img)
    img.src=img1
    img.alt='tesct '
+   return m
 }
 
 export default createContent
